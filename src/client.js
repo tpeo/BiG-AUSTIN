b7f2e4cecfb06867297ca762a32683058b335cde
aarushic <aarushichitagi@gmail.com>
2024-02-02 14:19:51 -0600

    nav bar

--- src/client.js
+++ src/client.js
@@ -3,5 +3,6 @@ import { createClient } from '@sanity/client'
 export default createClient({
     projectId: "39eecjq4",
     dataset: "production",
+    apiVersion: '2021-08-31',
     useCdn: true,
 })
